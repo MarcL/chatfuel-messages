2a66187f01e29f064b2f46857fa6fd94a6c73535
Marc Littlemore <marc.littlemore@gmail.com>
2018-05-24 19:14:51 +0100

    Add text and image messages to MessageBuilder

--- src/messageBuilder.js
+++ src/messageBuilder.js
@@ -1,4 +1,6 @@
 import BaseTemplate from './templates/baseTemplate';
+import Text from './templates/text';
+import Image from './templates/image';
 
 class MessageBuilder extends BaseTemplate {
     addMessage(message) {
@@ -7,6 +9,18 @@ class MessageBuilder extends BaseTemplate {
         }
 
         this.template.messages = [...this.template.messages, message.get()];
+
+        return this;
+    }
+
+    addTextMessage(text) {
+        this.addMessage(new Text(text));
+
+        return this;
+    }
+
+    addImageMessage(url) {
+        this.addMessage(new Image(url));
 
         return this;
     }
--- test/unit/messageBuilder.test.js
+++ test/unit/messageBuilder.test.js
@@ -8,29 +8,70 @@ describe('MessageBuilder', () => {
         expect(response).to.deep.equal({});
     });
 
-    it('should return expected response is one message is added', () => {
-        const response = new MessageBuilder()
-            .addMessage(new Text('test message'))
-            .get();
+    describe('addMessage()', () => {
+        it('should return expected response is one message is added', () => {
+            const response = new MessageBuilder()
+                .addMessage(new Text('test message'))
+                .get();
 
-        expect(response).to.deep.equal({
-            messages: [
-                { text: 'test message' },
-            ],
+            expect(response).to.deep.equal({
+                messages: [
+                    { text: 'test message' },
+                ],
+            });
+        });
+
+        it('should return expected response if two messages are added', () => {
+            const response = new MessageBuilder()
+                .addMessage(new Text('test message'))
+                .addMessage(new Text('test message 2'))
+                .get();
+
+            expect(response).to.deep.equal({
+                messages: [
+                    { text: 'test message' },
+                    { text: 'test message 2' },
+                ],
+            });
         });
     });
 
-    it('should return expected response is two messages are added', () => {
-        const response = new MessageBuilder()
-            .addMessage(new Text('test message'))
-            .addMessage(new Text('test message 2'))
-            .get();
+    describe('addTextMessage()', () => {
+        it('should return expected response if added', () => {
+            const givenTextMessage = 'given text message';
 
-        expect(response).to.deep.equal({
-            messages: [
-                { text: 'test message' },
-                { text: 'test message 2' },
-            ],
+            const response = new MessageBuilder()
+                .addTextMessage(givenTextMessage)
+                .get();
+
+            expect(response).to.deep.equal({
+                messages: [
+                    { text: givenTextMessage },
+                ],
+            });
+        });
+    });
+
+    describe('addImageMessage()', () => {
+        it('should return expected response if added', () => {
+            const givenUrl = 'https://given.url';
+
+            const response = new MessageBuilder()
+                .addImageMessage(givenUrl)
+                .get();
+
+            expect(response).to.deep.equal({
+                messages: [
+                    {
+                        attachment: {
+                            type: 'image',
+                            payload: {
+                                url: givenUrl,
+                            },
+                        },
+                    },
+                ],
+            });
         });
     });
 });
