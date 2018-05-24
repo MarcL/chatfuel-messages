aa1fc2533b1ec0538d73b2ba87517e3ee0c429e3
Marc Littlemore <marc.littlemore@gmail.com>
2018-05-24 22:22:27 +0100

    Add Mocha ESLint plugin

--- test/unit/templates/buttons/shareButton.test.js
+++ test/unit/templates/buttons/shareButton.test.js
@@ -1,9 +1,8 @@
 import ShareButton from '../../../../src/templates/buttons/shareButton';
 
-describe.only('Share button', () => {
+describe('Share button', () => {
     it('should return expected button response', () => {
-        const imageMessage = new ShareButton()
-            .get();
+        const imageMessage = new ShareButton().get();
 
         expect(imageMessage).to.deep.equal({
             type: 'element_share',
